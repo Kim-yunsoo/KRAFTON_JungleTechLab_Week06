c57bb8cbe16c3a223c84e4df4c3919575215ef6d
Teterschaft <gofla2242@tukorea.ac.kr>
2025-10-16 00:08:04 +0900

    Feature: FireBallActor 저장/불러오기 추가

--- TL2/FireBallActor.h
+++ TL2/FireBallActor.h
@@ -14,6 +14,12 @@ public:
 
     virtual void Tick(float DeltaTime) override;
 
+    void SetPointLightComponent(UPointLightComponent* InComponent) { PointLightComponent = InComponent; }
+    UPointLightComponent* GetPointLightComponent() const { return PointLightComponent; }
+
+    // Scene 로드 시 생성자가 만든 기본 컴포넌트를 삭제하기 위한 메서드
+    void ClearDefaultComponents();
+
 protected:
     ~AFireBallActor() override;
 
